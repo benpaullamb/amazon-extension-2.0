17208af3dc60d9d541e5d68099d91b233115b811
Ben <benpaullamb@gmail.com>
2022-03-07 20:04:09 +0000

    chore: fix privacy deploy

--- vite-privacy.config.js
+++ vite-privacy.config.js
@@ -6,6 +6,7 @@ import { resolve } from 'path';
 export default defineConfig({
   plugins: [react()],
   root: './src/privacy-policy',
+  base: '/amazon-extension-2.0/',
   build: {
     outDir: '../../docs',
     assetsDir: './',
